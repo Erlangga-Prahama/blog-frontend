62f23b338ac67677af34a68f700ac1664d646c04
unknown <Erlangga-Prahama>
2026-07-14 08:47:17 +0700

    membuat composaable usePosts

---
+++ src/composables/usePosts.js
@@ -0,0 +1,26 @@
+import { ref } from 'vue';
+import { useApi } from '@/composables/useApi'
+
+export function usePosts() {
+    const { loading, error, request, api } = useApi()
+    
+    const posts = ref([])
+    const meta = ref({
+        current_page: 1,
+        last_page: 1,
+        total: 0,
+    })
+
+    async function fetchPosts(params = {}) {
+        const data = await request(() => api.get('/posts', { params }))
+        
+        posts.value = data.data
+        meta.value = {
+            current_page: data.meta?.current_page ?? data.current_page ?? 1,
+            last_page: data.meta?.last_page ?? data.last_page ?? 1,
+            total: data.meta?.total ?? data.total ?? 0,
+        }
+
+        return {posts, meta, loading, fetchPosts}
+    }
+}
